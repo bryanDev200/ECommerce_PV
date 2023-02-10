a33b4520b52fe9153fc4d23b3ed1e7510662634c
stephLindegger <s_te_phy13@hotmaail.com>
2023-02-09 19:12:55 -0500

    clases steph

---
+++ ECommerce_EntityL/Models/Categories.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce_EntityL.Models
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+    }
+}
