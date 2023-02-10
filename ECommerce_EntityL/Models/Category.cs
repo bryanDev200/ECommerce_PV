a92a5c4cfc54b75f25340ba85db1674e25027e8e
stephLindegger <s_te_phy13@hotmaail.com>
2023-02-09 20:16:28 -0500

    correciones

--- ECommerce_EntityL/Models/Category.cs
+++ ECommerce_EntityL/Models/Category.cs
@@ -9,6 +9,8 @@ namespace ECommerce_EntityL.Models
     public class Category
     {
         public int Id { get; set; }
+
+        public HashSet<Subcategory> SubCategories{ get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime RegistrationDate { get; set; }
